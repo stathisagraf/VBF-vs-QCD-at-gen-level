4d19f09bf64e19b2576bf168231402d46f66246d
Efstathios Agrafiotis <105152484+stathisagraf@users.noreply.github.com>
2023-12-19 19:47:39 +0200

    Add files via upload

---
+++ pdg_id_to_charge.py
@@ -0,0 +1,22 @@
+import numpy as np
+import uproot 
+import awkward as ak
+import matplotlib.pyplot as plt
+from collections import Counter
+
+def charge(charg):
+    return 0
+    
+    
+vbf_path = 'perfNano_VBFHInv_PU200.l1ctlayer1.root'
+
+data = uproot.open(vbf_path)
+cha = []
+for dau in range(5):
+
+    cha.append(data['Events']['GenJets_dau{}_pdgId'.format(dau)].array())
+
+
+count = Counter(ak.flatten(ak.flatten(cha)))
+
+print(count)
